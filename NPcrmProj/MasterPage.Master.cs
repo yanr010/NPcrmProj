743f6ca35e22205846f91025e62f7dfdcfaf8707
Yan Rabinovich <yanr010@s.braude.ac.il>
2019-05-30 13:28:05 +0300

    commit

---
+++ NPcrmProj/MasterPage.Master.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace NPcrmProj
+{
+    public partial class MasterPage : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            if (!IsPostBack)
+            {
+                logdiv.Visible = false;
+            }
+            else
+            {
+                // בצע את שאר הפעולות
+            }
+            
+        }
+        
+        protected void enterclc(object sender, EventArgs e)
+        {
+            log.Visible = false;
+            logdiv.Visible = true;
+        }
+
+    }
+}
